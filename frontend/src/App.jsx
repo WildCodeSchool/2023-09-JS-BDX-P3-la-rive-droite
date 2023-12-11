bdf040d5d048a323270151ae80c936f2b640216f
Hepsox <marie.delaire@outlook.com>
2023-12-11 19:53:49 +0100

    card modele style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import ButtonMaxi from "./components/ButtonMaxi";
-import ButtonMini from "./components/ButtonMini";
-import CardModel from "./components/CardModel";
 
 function App() {
-  return (
-    <div>
-      <ButtonMaxi />
-      <div>coucou</div>
-      <ButtonMini />
-      <CardModel />
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
