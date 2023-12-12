4498229e53e6f413a1c2bd4a5d8d3035aff8dd9b
Hepsox <marie.delaire@outlook.com>
2023-12-12 17:05:53 +0100

    assemblage header + offre

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,9 @@
-import HeaderLong from "./components/HeaderLong";
-import AddDetailsCV from "./components/AddDetailsCV";
-import CompetenceSwitch from "./components/CompetenceSwitch";
 import "./App.css";
+
 import "./style/style_components/default-settings.css";
 
 function App() {
-  return (
-    <div>
-      <HeaderLong />
-      <h1>Hello world</h1>
-      <AddDetailsCV />
-      <CompetenceSwitch />
-    </div>
-  );
+  return <div>HEllo</div>;
 }
 
 export default App;
