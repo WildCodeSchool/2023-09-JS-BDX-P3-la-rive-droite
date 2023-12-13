54d1ba932e6cb5db85d7d83c013d2a2ea19a86be
Nassime HARMACH <Nassitch@hotmail.fr>
2023-12-13 11:51:50 +0100

    Add all prop-types conditions of components reusable

--- frontend/src/components/CheckboxCondition.jsx
+++ frontend/src/components/CheckboxCondition.jsx
@@ -1,11 +1,20 @@
-function CheckboxCondition() {
+import PropTypes from "prop-types";
+
+function CheckboxCondition({ textCondition }) {
   return (
     <div>
       <div className="container-checkbox">
         <input type="checkbox" id="scales" name="scales" />
-        <label htmlFor="scales">Case à cocher pour valider</label>
+        <label htmlFor="scales">
+          {textCondition ?? "Case à cocher pour valider"}
+        </label>
       </div>
     </div>
   );
 }
+
+CheckboxCondition.propTypes = {
+  textCondition: PropTypes.string.isRequired,
+};
+
 export default CheckboxCondition;
