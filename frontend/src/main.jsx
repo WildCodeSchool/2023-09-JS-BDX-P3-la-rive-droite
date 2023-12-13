b625f5c7f2c8f2ac948e8f082f9c695c754b3384
cassiopeelaurie <cassiopee.laurie@gmail.com>
2023-12-13 15:35:14 +0100

    Creating routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Import des composants pour les routes.
 import App from "./App";
+import CreateCV from "./pages/CV/CreateCV";
+import SignIn from "./pages/Connexion/SignIn";
+import LogIn from "./pages/Connexion/LogIn";
+import UserProfileModel from "./pages/ProfileUser/UserProfileModel";
+import AddExperience from "./pages/Experience/AddExperience";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/inscription",
+        element: <SignIn />,
+      },
+      {
+        path: "/connexion",
+        element: <LogIn />,
+      },
+      {
+        path: "/edit-profil",
+        element: <UserProfileModel />,
+      },
+      {
+        path: "/cv",
+        element: <CreateCV />,
+      },
+      {
+        path: "/experience",
+        element: <AddExperience />,
+      },
+    ],
   },
 ]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
--- frontend/src/pages/ProfileUser/UserProfileModel.jsx
+++ frontend/src/pages/ProfileUser/UserProfileModel.jsx
@@ -2,6 +2,7 @@ import Title from "../../components/Titles/Title";
 import HeaderLongUser from "../../components/Headers/HeaderLongUser";
 import Input from "../../components/Inputs/Input";
 import ButtonMaxi from "../../components/Boutons/ButtonMaxi";
+import CompetenceSwitch from "../../components/Competence Switch/CompetenceSwitch";
 
 function UserProfileUser() {
   return (
@@ -15,6 +16,7 @@ function UserProfileUser() {
         <Input titleInput="Mot de passe *" holderText="Mot de passe" />
         <Input titleInput="Téléphone *" holderText="Numéro de téléphone" />
         <Input titleInput="Addresse *" holderText="Adresse" />
+        <CompetenceSwitch />
         <ButtonMaxi />
       </div>
     </div>
