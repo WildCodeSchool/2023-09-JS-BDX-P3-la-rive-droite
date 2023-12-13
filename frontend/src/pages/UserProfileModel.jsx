16e7709e2e1454a54e85739ae4b3d5ccd9799573
cassiopeelaurie <cassiopee.laurie@gmail.com>
2023-12-13 11:43:50 +0100

    v3 userprofile

--- frontend/src/pages/UserProfileModel.jsx
+++ frontend/src/pages/UserProfileModel.jsx
@@ -2,23 +2,21 @@ import Title from "../components/Title";
 import HeaderLongUser from "../components/HeaderLongUser";
 import Input from "../components/Input";
 import ButtonMaxi from "../components/ButtonMaxi";
+import AddDetailsCV from "../components/AddDetailsCV";
 
 function UserProfileUser() {
   return (
     <div className="user-profile-page">
       <HeaderLongUser />
       <div>
-        <Title titleText="Vos coordonnées" holderText="coucou" />
-        <Input titleInput="Nom *" />
+        <Title titleText="Vos coordonnées" />
+        <Input titleInput="Nom *" holderText="ouehrfh" />
         <Input titleInput="Prénom *" />
         <Input titleInput="Email *" />
         <Input titleInput="Mot de passe *" />
         <Input titleInput="Téléphone *" />
         <Input titleInput="Addresse *" />
-        <h3> Votre CV </h3>
-        <div className="add-cv">
-          <i className="fa-solid fa-plus" />
-        </div>
+        <AddDetailsCV />
         <ButtonMaxi />
       </div>
     </div>
