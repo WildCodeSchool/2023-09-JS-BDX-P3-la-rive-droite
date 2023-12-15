d9ea5d13d98dc4de933dd999c8079b9af0d46e83
cassiopeelaurie <cassiopee.laurie@gmail.com>
2023-12-15 09:43:26 +0100

    offerpage

--- frontend/src/components/Headers/HeaderLongResearch.jsx
+++ frontend/src/components/Headers/HeaderLongResearch.jsx
@@ -1,10 +1,10 @@
 import "./header.css";
 import PropTypes from "prop-types";
-import Input from "../Inputs/Input";
+import InputSearch from "../Inputs/InputSearch";
 
 function HeaderLongResearch({ textTitle, textTitle2 }) {
   return (
-    <header className="header">
+    <header className="header with-round-bottom">
       <nav>
         <i className="fa-solid fa-arrow-left" />
         <i className="fa-solid fa-bars" />
@@ -12,9 +12,7 @@ function HeaderLongResearch({ textTitle, textTitle2 }) {
       <div className="header-content">
         <h1>{textTitle}</h1>
         <h2>{textTitle2}</h2>
-      </div>
-      <div className="input-research-container">
-        <Input className="input-research" showInput />
+        <InputSearch holderText="Recherche..." />
       </div>
     </header>
   );
