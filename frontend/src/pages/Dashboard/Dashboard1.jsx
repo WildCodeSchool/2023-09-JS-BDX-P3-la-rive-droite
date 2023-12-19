d23ac40f4b84455607e1a40df0d468ba1034f77c
Fred <druetfrederique27@gmail.com>
2023-12-19 16:12:06 +0100

    create page dash 2

--- frontend/src/pages/Dashboard/Dashboard1.jsx
+++ frontend/src/pages/Dashboard/Dashboard1.jsx
@@ -1,23 +1,31 @@
-import PropTypes from "prop-types";
+import { Outlet } from "react-router-dom";
 import RowDash from "../../components/Dashboards/RowDash";
 import TitleDashboard from "../../components/Dashboards/TitleDashboard";
 import ButtonMini from "../../components/Boutons/ButtonMini";
 
-function TitleDashboard1({ refAnnonce }) {
-  return (
+function Dashboard1() {
+  return window.location.pathname === "/dashboard" ||
+    window.location.pathname === "/dashboard/" ? (
     <div>
       <div className="title-btn">
         <h3 className="tab">Tableau de bord</h3>
         <ButtonMini textBtn="Ajouter une offre" />
       </div>
-      <h3 className="ref"> REF OFFRE : {refAnnonce} </h3>
-      <TitleDashboard />
+      <TitleDashboard
+        labelDash="ID Offres"
+        labelDash2="Entreprises"
+        labelDash3="Candidats"
+        labelDash4="Statut Offre"
+        labelDash5="Référent"
+        labelDash6="Actions"
+      />
       <RowDash />
+    </div>
+  ) : (
+    <div>
+      <Outlet />
     </div>
   );
 }
 
-TitleDashboard1.propTypes = {
-  refAnnonce: PropTypes.string.isRequired,
-};
-export default TitleDashboard1;
+export default Dashboard1;
---
+++ frontend/src/pages/Dashboard/Dashboard2.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import RowDash from "../../components/Dashboards/RowDash";
+import TitleDashboard from "../../components/Dashboards/TitleDashboard";
+import ButtonMini from "../../components/Boutons/ButtonMini";
+
+function Dashboard2({ refAnnonce }) {
+  return (
+    <div>
+      <div className="title-btn">
+        <h3 className="tab">Tableau de bord</h3>
+      </div>
+      <h3 className="ref"> REF OFFRE : {refAnnonce} </h3>
+      <h3>CANDIDATS</h3>
+      <div>
+        <ButtonMini textBtn="Envoyer" />
+        <TitleDashboard
+          labelDash="ID"
+          labelDash2="Noms"
+          labelDash3="Prénoms"
+          labelDash4="Tel"
+          labelDash5="Email"
+          labelDash6="CV"
+        />
+      </div>
+      <RowDash />
+    </div>
+  );
+}
+
+Dashboard2.propTypes = {
+  refAnnonce: PropTypes.string.isRequired,
+};
+export default Dashboard2;
