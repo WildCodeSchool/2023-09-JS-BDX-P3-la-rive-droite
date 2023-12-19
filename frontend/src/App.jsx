26d393c8afa92e108114b94e9115d7a8faf701f9
Fred <druetfrederique27@gmail.com>
2023-12-19 15:05:04 +0100

    offerdash-list-ok with some style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { Outlet } from "react-router-dom";
 import SignContextProvider from "./contexts/SignContext";
 import "./App.css";
 import Navbar from "./components/NavBar/NavBar";
-import RowDash from "./components/Dashboards/RowDash";
 
 function App() {
   return (
     <SignContextProvider>
       <Navbar />
       <Outlet />
-      <RowDash />
     </SignContextProvider>
   );
 }
