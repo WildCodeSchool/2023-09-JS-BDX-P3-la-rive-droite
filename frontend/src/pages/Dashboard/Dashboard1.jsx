4d2c0b0944a01cf0de7b79fbf8281d7f079f8b70
cassiopeelaurie <cassiopee.laurie@gmail.com>
2023-12-19 14:49:20 +0100

    ref + button add offer

--- frontend/src/pages/Dashboard/Dashboard1.jsx
+++ frontend/src/pages/Dashboard/Dashboard1.jsx
@@ -1,16 +1,21 @@
+import PropTypes from "prop-types";
 import TitleDashboard from "../../components/Dashboards/TitleDashboard";
 import ButtonMini from "../../components/Boutons/ButtonMini";
 
-function TitleDashboard1() {
+function TitleDashboard1({ refAnnonce }) {
   return (
     <div>
       <div className="title-btn">
         <h3 className="tab">Tableau de bord</h3>
         <ButtonMini textBtn="Ajouter une offre" />
       </div>
+      <h3 className="ref"> REF OFFRE : {refAnnonce} </h3>
       <TitleDashboard />
     </div>
   );
 }
 
+TitleDashboard1.propTypes = {
+  refAnnonce: PropTypes.string.isRequired,
+};
 export default TitleDashboard1;
