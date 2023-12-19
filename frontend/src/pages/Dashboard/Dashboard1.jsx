95758ef4af8170665ab1721aaf902abc64930cbd
cassiopeelaurie <cassiopee.laurie@gmail.com>
2023-12-19 12:04:35 +0100

    added prop textBtn for button mini

--- frontend/src/pages/Dashboard/Dashboard1.jsx
+++ frontend/src/pages/Dashboard/Dashboard1.jsx
@@ -1,9 +1,11 @@
 import TitleDashboard from "../../components/Dashboards/TitleDashboard";
+import ButtonMini from "../../components/Boutons/ButtonMini";
 
 function TitleDashboard1() {
   return (
     <div>
       <TitleDashboard />
+      <ButtonMini textBtn="Ajouter une offre" />
     </div>
   );
 }
