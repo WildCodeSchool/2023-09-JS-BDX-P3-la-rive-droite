0691b3225f05120bd8350edc88704e508cf805ad
Nassime HARMACH <Nassitch@hotmail.fr>
2023-12-20 10:57:17 +0100

    pushing for update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import App from "./App";
+import Home from "./pages/HomeOffer/Home";
 import SignIn from "./pages/Connexion/SignIn";
 import LogIn from "./pages/Connexion/LogIn";
-import Home from "./pages/HomeOffer/Home";
+import History from "./pages/Historique/History";
+import Favoris from "./pages/Favoris/Favoris";
 import UserProfileModel from "./pages/ProfileUser/UserProfileModel";
 import CreateCV from "./pages/CV/CreateCV";
 import AddExperience from "./pages/Experience/AddExperience";
 import AddFormation from "./pages/Formation/AddFormation";
-import Offer from "./pages/Offer/Offer";
-import History from "./pages/Historique/History";
-import Favoris from "./pages/Favoris/Favoris";
 import Dashboard1 from "./pages/Dashboard/Dashboard1";
 import Dashboard2 from "./pages/Dashboard/Dashboard2";
+import Offer from "./pages/Offer/Offer";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 const router = createBrowserRouter([
   {
