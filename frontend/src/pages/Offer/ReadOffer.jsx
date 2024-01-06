eb93305f62fdd697fd22d0308b0cc5e0623090c6
Nassitch <Nassitch@hotmail.fr>
2024-01-06 23:08:59 +0100

    Created and config component 'ReadOffer'.

--- frontend/src/pages/Offer/ReadOffer.jsx
+++ frontend/src/pages/Offer/ReadOffer.jsx
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import HeaderCourt from "../../components/Headers/HeaderCourt";
 import ButtonMaxi from "../../components/Boutons/ButtonMaxi";
-// Import de context.
-import { useUserContext } from "../../contexts/UserContext";
-import { useGlobalContext } from "../../contexts/GlobalContext";
-// Import messages d'erreurs.
-import ErrorMsg from "../../components/Alertes Messages/ErrorMsg";
-import SuccesMsg from "../../components/Alertes Messages/SuccesMsg";
 // Import du style.
 import "./read-offer.css";
 
 function ReadOffer() {
-  const { handleAddCv } = useUserContext();
-  const { errorMsg, succesMsg, msgContent } = useGlobalContext();
-
-  const [offers, setOffers] = useState([]);
+  const [offer, setOffer] = useState([]);
 
   useEffect(() => {
+    // const getOffer = async () => {
+    //   try {
+    //     const response = await fetch(`https://localhost:3310/api/offer/12`);
+    //     if (response.ok) {
+    //       const data = await response.json();
+    //       // console.log(data);
+    //       setOffers(data);
+    //       // console.log(offers);
+    //     } else {
+    //       console.error("Echec de la récupération des données.");
+    //     }
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
+    // };
+
+    // getOffer();
+
     const getOffer = async () => {
       try {
-        const response = await fetch(`https://localhost:3310/api/offer/12`);
-        if (response.ok) {
-          const data = await response.json();
-          // console.log(data);
-          setOffers(data);
-          // console.log(offers);
-        } else {
-          console.error("Echec de la récupération des données.");
-        }
+        const response = await axios.get("http://localhost:3310/api/offer/15");
+        setOffer(response.data);
       } catch (err) {
         console.error(err);
       }
@@ -39,29 +42,46 @@ function ReadOffer() {
   return (
     <>
       <HeaderCourt />
-      <div className="container-page with-rounded-border">
-        {offers.map((offer, key) => (
-          <div className="card-container" key={key.id}>
-            <div className="card-icons">
-              <div className="icon-view">
-                <i className="fa-regular fa-heart" />
-              </div>
-            </div>
 
-            <h3 className="label-offre">{offer.title}</h3>
-            <h4 className="entreprise-champs">{offer.company}</h4>
-            <h5 className="poste-champs">
-              {offer.type} - {offer.city} - Publiée le 24/11/2023
-            </h5>
-            <p className="p-description ">{offer.info}</p>
-            {/* <ButtonMini textBtn="Postuler" /> */}
-          </div>
-        ))}
+      <div className="container-page with-rounded-border offer-card">
         <div>
-          {errorMsg && <ErrorMsg message={msgContent} />}
-          {succesMsg && <SuccesMsg message={msgContent} />}
+          <h1 className="title-page">Postuler à l'offre</h1>
+          <p>Vous avez déjà postuler à cette offre le ...</p>
         </div>
-        <ButtonMaxi textBtn="Enregistrer" clickFunc={handleAddCv} />
+        <div className="card-container">
+          <div className="card-icons">
+            <div className="icon-view">
+              <i className="fa-regular fa-heart" />
+            </div>
+          </div>
+
+          <h3 className="title-offer">{offer.title}</h3>
+          <h4 className="company-offer">{offer.company}</h4>
+
+          <p className="description-word">Description</p>
+          <p className="p-description">{offer.info}</p>
+
+          <p className="sub-title">Type de contrat :</p>
+          <p className="info-offer">{offer.type}</p>
+
+          <p className="sub-title">Lieu :</p>
+          <p className="info-offer">{offer.city}</p>
+
+          <p className="sub-title">Profile recherché :</p>
+          <p className="info-offer">{offer.search_profile}</p>
+
+          <p className="sub-title">Lieu de travail :</p>
+          <p className="info-offer">{offer.work_place}</p>
+
+          <p className="sub-title">Salaire :</p>
+          <p className="info-offer">{offer.salary}</p>
+
+          <p className="sub-title">Informations supplémentaire.</p>
+          <p className="info-offer">{offer.info}</p>
+
+          <ButtonMaxi textBtn="Postuler" />
+          <ButtonMaxi textBtn="Contacter l'entreprise" />
+        </div>
       </div>
     </>
   );
