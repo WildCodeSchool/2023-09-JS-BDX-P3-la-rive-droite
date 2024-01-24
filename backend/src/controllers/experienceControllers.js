b171c1f4ec5021a8c4baf5f4f0493b9a74acf890
Fred <druetfrederique27@gmail.com>
2024-01-24 12:22:12 +0100

    refacto controllers try/catch

--- backend/src/controllers/experienceControllers.js
+++ backend/src/controllers/experienceControllers.js
@@ -5,102 +5,88 @@ const models = require("../models/index");
 //   return jwt.sign(data, process.env.APP_SECRET);
 // }
 
-const getExperiences = (_, res) => {
-  models.experience
-    .findAll()
-    .then((rows) => {
-      res.send(rows);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const getExperiencesByCvId = (req, res) => {
-  models.experience
-    .findAllByCvId(req.params.id)
-    .then((rows) => {
-      res.send(rows);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const getExperienceById = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-
-  models.experience
-    .findId(id)
-    .then(([item]) => {
-      if (item[0] != null) {
-        res.json(item[0]);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(422);
-    });
-};
-
-const postExperience = (req, res) => {
-  models.experience
-    .create(req.body)
-    .then((rows) => {
-      res.send({
-        id: rows.insertId,
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(422).send({ error: err.message });
-    });
-  // res.status(418).send(req.body)
-};
-
-const updateExperience = async (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (!id) {
+const getExperiences = async (_, res) => {
+  try {
+    const rows = await models.experience.findAll();
+    res.send(rows);
+  } catch (err) {
+    console.error(err);
     res.sendStatus(500);
   }
-
-  models.experience
-    .update(id, req.body)
-    .then((result) => {
-      if (result.affectedRows === 0) {
-        res.sendStatus(500);
-      }
-      res.sendStatus(200);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(422).send({ error: error.message });
-    });
 };
 
-const deleteExperienceById = (req, res) => {
-  const id = parseInt(req.params.id, 10);
+const getExperiencesByCvId = async (req, res) => {
+  try {
+    const rows = await models.experience.findAllByCvId(req.params.id);
+    res.send(rows);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+};
 
-  models.experience
-    .deleteId(id)
-    .then(([result]) => {
-      res.status(201).send({ message: result });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(422).send({ message: "Experience not found." });
+const getExperienceById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const [item] = await models.experience.findId(id);
+    if (item[0] != null) {
+      res.json(item[0]);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(422);
+  }
+};
+
+const postExperience = async (req, res) => {
+  try {
+    const rows = await models.experience.create(req.body);
+    res.send({
+      id: rows.insertId,
     });
+  } catch (err) {
+    console.error(err);
+    res.status(422).send({ error: err.message });
+  }
+};
+
+// const updateExperience = async (req, res) => {
+//   try {
+//     const id = parseInt(req.params.id, 10);
+//     if (!id) {
+//       res.sendStatus(500);
+//     }
+
+//     const result = await models.experience.update(id, req.body);
+//     if (result.affectedRows === 0) {
+//       res.sendStatus(500);
+//     }
+//     res.sendStatus(200);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(422).send({ error: error.message });
+//   }
+// };
+
+const deleteExperienceById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+
+    const [result] = await models.experience.deleteId(id);
+    res.status(201).send({ message: result });
+  } catch (err) {
+    console.error(err);
+    res.status(422).send({ message: "Experience not found." });
+  }
 };
 
 module.exports = {
   getExperiences,
   getExperienceById,
   postExperience,
-  updateExperience,
+  // updateExperience,
   deleteExperienceById,
   getExperiencesByCvId,
 };
